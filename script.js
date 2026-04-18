c0f8c631052e0ee0e7f244038c03dbc7694e7196
BunBwon <161734203+BunBwon@users.noreply.github.com>
2026-04-18 21:39:57 +0300

    bough fix, add scrip.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function ROBUUX() {
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}    
+
+// Add event listener to the button after the DOM loads
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("ropuksgetter9000").addEventListener("click", ROBUUX);
+});
